5d600aee4cb1d419da0481d63b1e41996b9dde01
Wyatt Snyder <wyatt.james.snyder@gmail.com>
2021-02-20 20:34:23 -0600

    initial commit

---
+++ envSetup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+echo "Installing Programs"
+
+sudo apt install shotwell # background slideshow
+sudo apt install htop     # system monitor
---
+++ wallpaperChanger/wallpaperChanger.py
@@ -0,0 +1,5 @@
+import os
+
+base_command = "/use/bin/gsettings set org.gnome.desktop.background picture-uri /home/wyatt/Pictures/Wallpapers/"
+
+
